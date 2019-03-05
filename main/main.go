3d160af53a20482b95a4568a579a7712ac188c6e
boreq <boreq@0x46.net>
2019-03-05 22:16:53 +0100

    Add a verbosity option, reduce default verbosity

--- main/main.go
+++ main/main.go
@@ -6,12 +6,42 @@ import (
 	"os"
 
 	"github.com/boreq/guinea"
+	"github.com/boreq/plum/logging"
 	"github.com/boreq/plum/main/commands"
 )
 
 func main() {
+	injectGlobalBehaviour(&commands.MainCmd)
 	if err := guinea.Run(&commands.MainCmd); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+var globalOptions = []guinea.Option{
+	guinea.Option{
+		Name:        "verbosity",
+		Type:        guinea.String,
+		Default:     "info",
+		Description: "One of: debug, info, warn, error or crit. Default: info",
+	},
+}
+
+func injectGlobalBehaviour(cmd *guinea.Command) {
+	cmd.Options = append(cmd.Options, globalOptions...)
+	oldRun := cmd.Run
+	cmd.Run = func(c guinea.Context) error {
+		level, err := logging.LevelFromString(c.Options["verbosity"].Str())
+		if err != nil {
+			return err
+		}
+		logging.SetLoggingLevel(level)
+		if oldRun != nil {
+			return oldRun(c)
+		}
+		return nil
+	}
+	for _, subCmd := range cmd.Subcommands {
+		injectGlobalBehaviour(subCmd)
 	}
 }
