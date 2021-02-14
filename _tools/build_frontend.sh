b518e077bb3ee5229157affbd8cf694c7b374a14
boreq <boreq@0x46.net>
2021-02-14 15:49:12 +0100

    Update frontend
    Persist volume, shuffle and muted on page reload

--- _tools/build_frontend.sh
+++ _tools/build_frontend.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 set -e
 
-# Build frontend
-echo "Running yarn build"
+versionFile="ports/http/frontend/version.go"
+previousCommit=$(cat ${versionFile} | grep -Eo '[0-9a-f]{32}')
+
+
 cd ../eggplant-frontend
+
+if ! git diff-index --quiet HEAD --; then
+    echo "You have uncommited changes in the frontend repository!"
+    exit 1
+fi
+
+commit=$(git rev-parse HEAD)
+echo "Current frontend commit: ${commit}"
+
+echo "Generating a commit message ${previousCommit} -> ${commit}"
+
+commitFile="/tmp/eggplant-frontend-commit.txt"
+touch ${commitFile}
+echo "Update frontend" > ${commitFile}
+echo "" >> ${commitFile}
+git log --pretty=format:%s ${previousCommit}..${commit} >> ${commitFile}
+
+echo "Building the frontend"
 rm -rf dist
 yarn build
 
-# Build backend
-cd ../eggplant/ports/http/frontend
+cd ../eggplant
+
+if ! git diff-index --quiet HEAD --; then
+    echo "You have uncommited changes in the backend repository!"
+    exit 1
+fi
+
 echo "Running https://github.com/rakyll/statik"
+cd ./ports/http/frontend
 statik -f -src=../../../../eggplant-frontend/dist
+cd ../../../
+
+echo "Persisting frontend version"
+
+echo "package frontend" > "${versionFile}"
+echo "" >> "${versionFile}"
+echo "const FrontendCommit = \"${commit}\"" >> "${versionFile}"
+
+git add -u
+git commit -F ${commitFile}
---
+++ ports/http/frontend/version.go
@@ -0,0 +1,3 @@
+package frontend
+
+const FrontendCommit = "c42982dd270374b7085f357deb2e7175a3aa4d5d"
