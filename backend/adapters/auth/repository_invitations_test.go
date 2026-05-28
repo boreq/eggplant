6345c50a2cd37dcf282cbf15d881aa1d753bcbfe
boreq <boreq@0x46.net>
2026-05-28 19:00:28 +0200

    Tests use a transaction provider

--- backend/adapters/auth/repository_invitations_test.go
+++ backend/adapters/auth/repository_invitations_test.go
@@ -4,16 +4,14 @@ import (
 	"testing"
 	"time"
 
-	"github.com/boreq/eggplant/adapters/auth"
 	app "github.com/boreq/eggplant/application/auth"
 	authdomain "github.com/boreq/eggplant/domain/auth"
 	"github.com/boreq/eggplant/internal/fixture"
 	"github.com/stretchr/testify/require"
-	bolt "go.etcd.io/bbolt"
 )
 
 func TestRemoveOldInvitationsWhenSavingNewOnes(t *testing.T) {
-	db, cleanup := fixture.Bolt(t)
+	tp, cleanup := fixture.AuthTransactionProvider(t)
 	defer cleanup()
 
 	token1, err := authdomain.NewInvitationTokenFromString("1")
@@ -27,30 +25,24 @@ func TestRemoveOldInvitationsWhenSavingNewOnes(t *testing.T) {
 	invitation2 := authdomain.NewInvitation(token2, time.Now())
 	invitation3 := authdomain.NewInvitation(token3, time.Now())
 
-	err = db.Update(func(tx *bolt.Tx) error {
-		r, err := auth.NewInvitationRepository(tx)
-		require.NoError(t, err)
-
+	err = tp.Write(func(r *app.TransactableRepositories) error {
 		for _, invitation := range []authdomain.Invitation{invitation1, invitation2, invitation3} {
-			err := r.Put(invitation)
-			require.NoError(t, err)
+			if err := r.Invitations.Put(invitation); err != nil {
+				return err
+			}
 		}
-
 		return nil
 	})
 	require.NoError(t, err)
 
-	err = db.View(func(tx *bolt.Tx) error {
-		r, err := auth.NewInvitationRepository(tx)
-		require.NoError(t, err)
-
-		_, err = r.Get(invitation1.Token())
+	err = tp.Read(func(r *app.TransactableRepositories) error {
+		_, err := r.Invitations.Get(invitation1.Token())
 		require.ErrorIs(t, err, app.ErrNotFound)
 
-		_, err = r.Get(invitation2.Token())
+		_, err = r.Invitations.Get(invitation2.Token())
 		require.NoError(t, err)
 
-		_, err = r.Get(invitation3.Token())
+		_, err = r.Invitations.Get(invitation3.Token())
 		require.NoError(t, err)
 
 		return nil
